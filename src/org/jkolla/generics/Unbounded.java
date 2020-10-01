2b06b1ae80dbe623be89efdc0ac50be2d380d9ad
Jogeswararao Kolla <jogeskolla@gmail.com>
2020-10-01 12:58:55 -0700

    add the unbounded wildcard ? list example

---
+++ src/org/jkolla/generics/Unbounded.java
@@ -0,0 +1,19 @@
+package org.jkolla.generics;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+public class Unbounded {
+    public static void main(String[] args) {
+        List<?> values = Arrays.asList("Hello", LocalDate.now(),20);
+        System.out.println(values.size());
+        values.forEach((Object o) -> System.out.println(o));
+       // values.add(10); // we get compile error as we can't add elements to ? unknown type.
+        List<Integer> integers = Arrays.asList(10,30,40);
+        System.out.println(integers); // [10, 30, 40]
+        // integers.add(50); // java.lang.UnsupportedOperationException occurs we can't add elements to integers because Arrays.asList() creates an unmodifiable list
+        integers.set(1,50);
+        System.out.println(integers); // [10, 50, 40]
+    }
+}
