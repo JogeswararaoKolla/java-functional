ba0050af33d27e8dbf7003a06f3a807d0a84fea3
Jogeswararao Kolla <jogeskolla@gmail.com>
2020-10-04 08:39:04 -0700

    added grouping movies by year and get the max rated movie name by re-mapping using toMap

--- src/org/jkolla/collectors/mappingDemo.java
+++ src/org/jkolla/collectors/mappingDemo.java
@@ -2,28 +2,27 @@ package org.jkolla.collectors;
 
 import org.jkolla.models.Movie;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class mappingDemo {
-    public static List<Movie> createMovies(){
-        return new ArrayList<>(){{
-            add(new Movie("Rangasthalam","2018", 8.4, Arrays.asList("Ram Charan","Samantha Ruth Prabhu", "Aadhi", "Jagapathi Babu")));
-            add(new Movie("Bharat Ane Nenu","2018", 7.7,Arrays.asList("Mahesh Babu", "Kiara Advani", "Prakash Raj" )));
-            add(new Movie("Saaho","2019", 5.2,Arrays.asList("Prabhas", "Shraddha Kapoor", "Jackie Shroff")));
-            add(new Movie("Bahubali: The Beginning","2015", 8.1,Arrays.asList( "Prabhas", "Rana Daggubati", "Ramya Krishnan" )));
-            add(new Movie("Baahubali 2: The Conclusion","2017", 8.2,Arrays.asList( "Prabhas", "Rana Daggubati", "Anushka Shetty")));
-            add(new Movie("Sye Raa Narasimha Reddy","2019", 7.5,Arrays.asList( "Chiranjeevi", "Amitabh Bachchan", "Sudeep" )));
-            add(new Movie("Ala Vaikunthapurramuloo","2020", 7.2,Arrays.asList( "Allu Arjun", "Pooja Hegde", "Tabu" )));
-            add(new Movie("Nannaku Prematho","2016", 7.6,Arrays.asList( "N.T. Rama Rao Jr.", "Rakul Preet Singh", "Jagapathi Babu" )));
-            add(new Movie("Arjun Reddy","2017", 8.1,Arrays.asList( "Vijay Deverakonda", "Shalini Pandey", "Jia Sharma" )));
+    public static List<Movie> createMovies() {
+        return new ArrayList<>() {{
+            add(new Movie("Rangasthalam", "2018", 8.4, Arrays.asList("Ram Charan", "Samantha Ruth Prabhu", "Aadhi", "Jagapathi Babu")));
+            add(new Movie("Bharat Ane Nenu", "2018", 7.7, Arrays.asList("Mahesh Babu", "Kiara Advani", "Prakash Raj")));
+            add(new Movie("Saaho", "2019", 5.2, Arrays.asList("Prabhas", "Shraddha Kapoor", "Jackie Shroff")));
+            add(new Movie("Bahubali: The Beginning", "2015", 8.1, Arrays.asList("Prabhas", "Rana Daggubati", "Ramya Krishnan")));
+            add(new Movie("Baahubali 2: The Conclusion", "2017", 8.2, Arrays.asList("Prabhas", "Rana Daggubati", "Anushka Shetty")));
+            add(new Movie("Sye Raa Narasimha Reddy", "2019", 7.5, Arrays.asList("Chiranjeevi", "Amitabh Bachchan", "Sudeep")));
+            add(new Movie("Ala Vaikunthapurramuloo", "2020", 7.2, Arrays.asList("Allu Arjun", "Pooja Hegde", "Tabu")));
+            add(new Movie("Nannaku Prematho", "2016", 7.6, Arrays.asList("N.T. Rama Rao Jr.", "Rakul Preet Singh", "Jagapathi Babu")));
+            add(new Movie("Arjun Reddy", "2017", 8.1, Arrays.asList("Vijay Deverakonda", "Shalini Pandey", "Jia Sharma")));
         }};
     }
+
     public static void main(String[] args) {
-        List<Movie> movies= createMovies();
+        List<Movie> movies = createMovies();
         Map<String, List<Movie>> moviesByReleaseYear = movies.stream()
                 .collect(Collectors.groupingBy(Movie::getMovieReleaseYear));
         System.out.println("moviesByReleaseYear = " + moviesByReleaseYear);
@@ -49,6 +48,33 @@ public class mappingDemo {
                         entry -> entry.getValue().stream().map(e -> e.getRating()).collect(Collectors.toList())
                 ));
         System.out.println("moviesByReleaseYear3 = " + moviesByReleaseYear3);
-        
+
+        // grouping movies by year and get the max rated movie name.
+        Map<String, Optional<Movie>> movieByYearRating = movies.stream()
+                .collect(Collectors.groupingBy(Movie::getMovieReleaseYear,
+                        Collectors.maxBy(Comparator.comparing(Movie::getRating))));
+        System.out.println("movieByYearRating = " + movieByYearRating);
+
+        Map<String, Optional<String>> movieNameByYearRating = movieByYearRating
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        entry -> entry.getKey(),
+                        entry -> entry.getValue().map(e -> e.getMovieName())
+                ));
+        System.out.println("movieNameByYearRating = " + movieNameByYearRating);
+
+        Optional<Map.Entry<String, String>> collect = movieByYearRating
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        entry -> entry.getKey(),
+                        entry -> entry.getValue().map(e -> e.getMovieName()).orElse("No Movie")
+                )).entrySet()
+                .stream()
+                .collect(Collectors.maxBy(Map.Entry.comparingByKey()));
+        System.out.println("collect = " + collect); // collect = Optional[2020=Ala Vaikunthapurramuloo]
     }
 }
+
+
