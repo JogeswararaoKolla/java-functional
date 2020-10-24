c27abf1d8d3309c0f1a4ba23c80fabb4509a0b7c
Jogeswararao Kolla <jogeskolla@gmail.com>
2020-10-24 01:37:09 -0700

    merging of maps using Map Interface Factory merge method

--- src/org/jkolla/collections/MapMergeDemo.java
+++ src/org/jkolla/collections/MapMergeDemo.java
@@ -1,27 +1,12 @@
 package org.jkolla.collections;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
-import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
 
 public class MapMergeDemo {
     public static void main(String[] args) {
-        List<Integer> numbers = Stream.generate(() -> Math.random())
-                .mapToInt(p -> (int) (p * 1000))
-               // .boxed() // Stream<Integer>
-                .mapToObj(Integer::valueOf)
-                .limit(10)
-                .collect(toList());
-        System.out.println("numbers = " + numbers); // numbers = [850, 970, 314, 303, 951, 913, 551, 345, 213, 329]
-
-//        Stream<Integer>	boxed()
-//        Returns a Stream consisting of the elements of this stream, each boxed to an Integer.
-//        <U> Stream<U>	mapToObj(IntFunction<? extends U> mapper)
-//        Returns an object-valued Stream consisting of the results of applying the given function to the elements of this stream.
 
         Map<Integer,String> map1 = new HashMap<>();
         map1.put(850,"Broccoli");
@@ -32,17 +17,45 @@ public class MapMergeDemo {
         map2.put(970,"Cabbage");
         map2.put(314,"Asparagus");
         map2.put(551,"Lettuce");
+        map2.put(303,"Tomato");
 
-        // Merging Map2 to Map1 if both keys match to a comma separated list of values from both maps.
-        for( Map.Entry<Integer,String> entry : map1.entrySet()) {
-            if(map2.containsKey(entry.getKey()))
-                entry.setValue(entry.getValue() + "," + map2.get(entry.getKey()));
-                System.out.println("entry = " + entry);
-        }
+        Map<Integer,String>  map3 = new HashMap<>(map1);
+        System.out.println("map3 = " + map3);
 
-        System.out.println("Merging by using entry.setValue() method map1 = " + map1); // Merging by using entry.setValue() method map1 = {850=Broccoli, 970=Carrots,Cabbage, 314=Spinach,Asparagus, 303=Tomato}
+        Map<Integer,String>  map4 = new HashMap<>(map2);
+        System.out.println("map4 = " + map4);
 
-        // Merging Map2 to Map1 if the keys are same, if key does'nt exists it copies  to a comma separated list of values from both maps.
+// Merging map4 to map3 using computeIfPresent, computeIfAbsent methods
+// default V	computeIfAbsent(K key, Function<? super K,? extends V> mappingFunction)
+//If the specified key is not already associated with a value (or is mapped to null), attempts to compute its value using the given mapping function and enters it into this map unless null.
+//default V	computeIfPresent(K key, BiFunction<? super K,? super V,? extends V> remappingFunction)
+//If the value for the specified key is present and non-null, attempts to compute a new mapping given the key and its current mapped value.
+//
+
+        map4.forEach((key,value) -> {
+         if(map3.containsKey(key))
+             map3.computeIfPresent(key, (existingKey,oldValue) -> {
+                 System.out.println("Inside computeIfPresent");
+                 System.out.println("existingKey = " + existingKey);
+                 System.out.println("oldValue = " + oldValue);
+                 return   oldValue + "," + value;
+             });
+         else
+         map3.computeIfAbsent(key,e -> {
+             System.out.println("Inside the computeIfAbsent");
+             System.out.println(e);
+             System.out.println(value);
+             return value;
+         });
+
+     });
+
+ System.out.println("After Compute method calls map3= " + map3);
+
+// default V merge(K key, V value, BiFunction<? super V, ? super V, ? extends V> remappingFunction)
+// If the specified key is not already associated with a value or is associated with null, associates it with the given non-null value.
+// Merging Map2 to Map1 if the keys are same, if key doesn't exists it copies  to a comma separated list of values from both maps.
+
         map2.forEach((k2,v2) -> {
             map1.merge(k2, v2, new BiFunction<String, String, String>() {
                 @Override
@@ -52,9 +65,6 @@ public class MapMergeDemo {
             });
         });
         System.out.println("After calling merge method map1 = " + map1); // After calling merge method map1 = {850=Broccoli, 970=Carrots,Cabbage,Cabbage, 314=Spinach,Asparagus,Asparagus, 303=Tomato}
-
-//        default V	merge​(K key, V value, BiFunction<? super V,​? super V,​? extends V> remappingFunction)
-//        If the specified key is not already associated with a value or is associated with null, associates it with the given non-null value.
 
     }
 }
