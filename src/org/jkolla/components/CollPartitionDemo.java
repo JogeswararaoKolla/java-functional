7910ca557b3093f11e02f376688a6827264f8343
Jogeswararao Kolla <jogeskolla@gmail.com>
2020-11-26 12:08:22 -0700

    Collection<T> partition using Predicate<T> demo

--- src/org/jkolla/components/CollPartitionDemo.java
+++ src/org/jkolla/components/CollPartitionDemo.java
@@ -1,6 +1,7 @@
 package org.jkolla.components;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CollPartitionDemo {
@@ -14,8 +15,10 @@ public class CollPartitionDemo {
             add("Vasanthi");
         }};
 
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        UtilFunction<Integer> partitionNumbers = new UtilFunction<>();
+        System.out.println("Even Numbers : " + partitionNumbers.getCollSatisfyingCondition(numbers,e -> e % 2 == 0 )); // Even Numbers : [2, 4, 6, 8, 10]
         UtilFunction<String> partition = new UtilFunction<>();
-        System.out.println("partition = " + partition.getClass());
         System.out.println("namesStartsWithV = " + partition.getCollSatisfyingCondition(names,s -> s.startsWith("V") || s.startsWith("S"))); // namesStartsWithV = [Suma, Vanaja, Vasanthi]
         System.out.println("namesStartsWithJ = " + partition.getCollSatisfyingCondition(names,s -> s.startsWith("J"))); // namesStartsWithJ = [Jogi]
         System.out.println("namesStartsWithH = " + partition.getCollSatisfyingCondition(names,s -> s.startsWith("H"))); // namesStartsWithH = [Humisha]
