adb3812dbc4bda0e89374bc224b929780067532c
Jogeswararao Kolla <jogeskolla@gmail.com>
2020-12-09 01:21:21 -0700

    Scan Component generate keys using Collector

--- src/org/jkolla/components/ScanTemporaryType.java
+++ src/org/jkolla/components/ScanTemporaryType.java
@@ -1,2 +1,28 @@
-package org.jkolla.components;public class ScanTemporaryType {
+package org.jkolla.components;
+
+import org.jkolla.models.TransactionEvent;
+
+import java.time.LocalDate;
+
+public class ScanTemporaryType extends TransactionEvent {
+    int count;
+
+    public ScanTemporaryType(int count) {
+        this.count = count;
+    }
+
+    public ScanTemporaryType(LocalDate dt, String kind, Double amount, int count) {
+        super(dt, kind, amount);
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "ScanTemporaryType{" +
+                "count=" + count + ", " +
+                "dt=" + super.getDt() + ", " +
+                "kind=" + super.getKind() +  ", " +
+                "amount=" + super.getAmount() +
+                '}';
+    }
 }
--- src/org/jkolla/models/TransactionEvent.java
+++ src/org/jkolla/models/TransactionEvent.java
@@ -1,2 +1,59 @@
-package org.jkolla.models;public class TransactionEvent {
+package org.jkolla.models;
+
+import java.time.LocalDate;
+
+public class TransactionEvent {
+
+  private  LocalDate dt;
+  private  String kind;
+  private  Double amount;
+
+    public TransactionEvent() {
+    }
+
+    public void setDt(LocalDate dt) {
+        this.dt = dt;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public void setTransactionEvent (TransactionEvent transactionEvent) {
+        this.dt = transactionEvent.dt;
+        this.kind = transactionEvent.kind;
+        this.amount = transactionEvent.amount;
+    }
+
+    public TransactionEvent(LocalDate dt, String kind, Double amount) {
+        this.dt = dt;
+        this.kind = kind;
+        this.amount = amount;
+    }
+
+    public LocalDate getDt() {
+        return dt;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "TransactionEvent{" +
+                "dt=" + dt +
+                ", kind='" + kind + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
+
